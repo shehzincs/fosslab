9a3e79f7682a24c7ef092c704583bce47bed0a0f
shehzincs <shehzincs@gmail.com>
2018-02-27 15:04:32 +0530

    Shell Basics

---
+++ shellbasics.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Home Directory : $HOME"
+echo "Operating System : $OSTYPE"
+echo "Shell Using Now : $SHELL"
+echo "Current User : $USER"
+echo "Currently Logged on User :"
+who
+dir=$PWD
+echo "Current Working Directory : $dir"
+
+
+
+
+
+
